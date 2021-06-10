252d5df14b646e13de6e868cebf04cc658dbf75d
Shaunak G <hello@srg.codes>
2021-06-10 21:46:26 +1000

    deploy to GAE

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ app.use(require('cors')({
   origin: "https://sixtysecondsofpython.srg.id.au"
 }))
 
+const rateLimit = require("express-rate-limit");
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+//  apply to all requests
+app.use(limiter);
+
 let ips = [];
 
 const supported_commands = [

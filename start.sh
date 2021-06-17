7cf7cb70542ad199ced9b11616b0c93cc63335a2
Shaunak G <hello@srg.codes>
2021-06-17 17:33:30 +1000

    just make it one line

--- start.sh
+++ start.sh
@@ -5,10 +5,4 @@
 BUILD_ID = $(sudo docker build .)
 
 ## Run image
-sudo docker run -d \ # In detached mode
-                # Allow the image to have access to the docker on our machine
-                -v /var/run/docker.sock:/var/run/docker.sock \
-                # Forward HTTP port 80 on the container to our port 80
-                -p 80:80 \
-                # Use the build we made before.
-                "${BUILD_ID}"
+sudo docker run -d -v /var/run/docker.sock:/var/run/docker.sock -p 80:80 "${BUILD_ID}"

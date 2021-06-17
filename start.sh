bf7deea4710241f0695f2c0af620e2ee77a5f9e4
Shaunak G <hello@srg.codes>
2021-06-17 17:32:51 +1000

    fix script

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ BUILD_ID = $(sudo docker build .)
 
 ## Run image
 sudo docker run -d \ # In detached mode
-                -v /var/run/docker.sock:/var/run/docker.sock \ # Allow the image to have access to the docker on our machine
-                -p 80:80 # Forward HTTP port 80 on the container to our port 80
-                "${BUILD_ID}" # Use the build we made before.
+                # Allow the image to have access to the docker on our machine
+                -v /var/run/docker.sock:/var/run/docker.sock \
+                # Forward HTTP port 80 on the container to our port 80
+                -p 80:80 \
+                # Use the build we made before.
+                "${BUILD_ID}"
